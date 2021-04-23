50d4d7aa582990c934b00406f245bdf1b3216b95
PSoysauce <psoysauce@gmail.com>
2021-04-23 00:56:05 -0500

    I updated the style for the oscilloscope, power, and voltage page. and added functionality to the power supply and pause/export functions

--- src/js/power.js
+++ src/js/power.js
@@ -12,9 +12,21 @@ data: {
     }],
 },
 options: {
+    plugins: {
+        zoom: {
+          pan: {
+            enabled: true,
+            mode: "x",
+          },
+          zoom: {
+            enabled: true,
+            mode: "x",
+          },
+        },
+      },
     title: {
         display: true,
-        text: 'Volt Meter',
+        text: 'Power Supply',
         fontColor: 'rgb(255, 255, 255)',
     },
     legend: {
@@ -47,10 +59,13 @@ options: {
             realtime: {
                 onRefresh: function(chart) {
                     chart.data.datasets.forEach(function(dataset) {
+                        xVal = Date.now();
+                        yVal = Math.random();
                         dataset.data.push({
-                            x: Date.now(),
-                            y: 2,
+                        x: xVal,
+                        y: yVal,
                         });
+                        storeChartData(xVal, yVal);
                     });
                 },
                 delay: 2000,
@@ -58,3 +73,46 @@ options: {
         }]
     },
 }});
+
+// toggle the data on
+function onData() {
+    chartData = [];
+    chart.options.plugins.streaming.pause = false;
+    chart.update();
+    display_data = true;
+}
+
+// toggle the data off
+function offData() {
+    chart.options.plugins.streaming.pause = true;
+    chart.update();
+    display_data = false;
+}
+
+chartData = [];
+
+function storeChartData(xVal, yVal) {
+  chartData.push({ x: xVal, y: yVal });
+}
+
+function convertToCSV() {
+  const csvRows = [];
+  const headers = ["Time,Voltage"];
+  csvRows.push(headers.join(","));
+  for (const row of chartData) {
+    csvRows.push([row.x, row.y].join(","));
+  }
+  download(csvRows.join("\n"));
+}
+
+// how to download the data to a csv
+const download = function (data) {
+  const blob = new Blob([data], { type: "text/csv" });
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.setAttribute("href", url);
+  a.setAttribute("download", "download.csv");
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+};
